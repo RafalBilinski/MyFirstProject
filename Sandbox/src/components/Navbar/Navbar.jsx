3b2a19aea8de509f0dbca03e22f56ba073d326f7
Rafal Bilinski <rafal.bilinski92@gmail.com>
2025-04-10 12:29:28 +0200

    Updated Navbar component to create dropdown menu for language and project selection. Updated main.jsx to include the new Polish resume component.

--- Sandbox/src/components/Navbar/Navbar.jsx
+++ Sandbox/src/components/Navbar/Navbar.jsx
@@ -2,47 +2,85 @@ import "./navbar.css";
 import React from "react";
 import Logo from "../../assets/react.svg?react";
 
-
-// Navbar Component
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      activeDropdown: null
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.handleSubmenuClick = this.handleSubmenuClick.bind(this);
   }
 
-// Obsługa zdarzeń
-  handleClick(e) {
-    const component = e.target.getAttribute("component"); // Pobranie nazwy komponentu
-    console.log(`Button clicked: ${component}`);
+  // Handle regular button clicks
+  handleClick(e, component) {
     if (this.props.onButtonClick) {
-      this.props.onButtonClick(component); // Wywołanie funkcji przekazanej w props
+      this.props.onButtonClick(component);
     }
   }
 
-  generateButtons() {
-    const { navbarItems } = this.props; // Pobieranie obiektu navbarItems z props
-    return navbarItems.map((item) => (
-      <a href={`#${item.id}`} key={item.id} className="navbar-link">
-        <button
-          className="navbar-btn"
-          id={item.id + "-btn"} // Ustawienie id przycisku
-          component={item.component} // Przechowuje nazwę komponentu
-          onClick={this.handleClick}
-        >
-          {item.name} {/* Wyświetlanie nazwy przycisku */}
-        </button>
-      </a>
-    ));
+  // Handle Projects button click
+  handleSubmenuClick(e, id) {
+    e.preventDefault();
+    this.setState(prevState => ({
+      activeDropdown: prevState.activeDropdown === id ? null : id
+    }));
   }
 
-// Wymagana metoda renderująca
+  // Render buttons
+  renderButton(item) {
+    if (item.isDropdown) {
+      return (
+        <div 
+          key={item.id} 
+          className="navbar-link"
+        >
+          <div className="dropdown-container">
+            <button 
+              className="navbar-btn" 
+              id={item.id + "-btn"}
+              onClick={(e) => this.handleSubmenuClick(e, item.id)}
+            >
+              {item.name}
+            </button>
+            {this.state.activeDropdown === item.id && (
+              <div className="dropdown-content"
+              onMouseLeave={() => this.setState({ activeDropdown: null })}>
+                {item.submenu.map(subItem => (
+                  <button
+                    key={subItem.id}
+                    className="navbar-btn"
+                    onClick={(e) => this.handleClick(e, subItem.component)}
+                  >
+                    {subItem.name}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div 
+        key={item.id} 
+        className="navbar-link"
+        onClick={(e) => this.handleClick(e, item.component)}
+      >
+        <button className="navbar-btn" id={item.id + "-btn"}>
+          {item.name}
+        </button>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div className="navbar" id="navbar">
-        {this.generateButtons()}
-        <Logo className="logo" id="react-logo" />
-      </div>
+      <nav className="navbar">
+        {this.props.navbarItems.map(item => this.renderButton(item))}
+        <Logo className="logo" />
+      </nav>
     );
   }
 }
